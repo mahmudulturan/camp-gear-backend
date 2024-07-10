4c91130691728e3d756bb4464421694b8e4f87c3
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-10 16:32:49 +0600

    add: products interface

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,25 @@
+
+export interface IPrice {
+    amount: number;
+    discount: number;
+};
+
+export interface IRating {
+    rate: number;
+    count: number;
+}
+export interface IInventory {
+    quantity: number;
+    inStock: boolean;
+};
+
+export interface IProduct {
+    title: string;
+    description: string;
+    price: IPrice;
+    category: string;
+    image: string[];
+    rating: IRating;
+    inventory: IInventory;
+}
+
