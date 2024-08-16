e45bf3d68fd1d8c3f8c06957e2edcd62e9166e52
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-17 02:37:08 +0600

    add: cors origin url

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import router from './app/routes';
+import configs from './app/configs';
 
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: [configs.local_frontend_url as string, configs.live_frontend_url as string] }));
 
 
 app.use('/api', router);
--- src/app/configs/index.ts
+++ src/app/configs/index.ts
@@ -7,4 +7,6 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 export default {
     port: process.env.PORT,
     db_url: process.env.DB_URL,
+    live_frontend_url: process.env.LIVE_FRONTEND_URL,
+    local_frontend_url: process.env.LOCAL_FRONTEND_URL
 }
